f3b6646503ca1d804ae4cec23d8b5e0579f9a2e2
JulioMourajr <juliomourajr@hotmail.com>
2023-04-04 16:34:02 -0300

    Pagina de curriculo com as seçoes dinamicas.

--- src/components/Curriculo/Curriculo.jsx
+++ src/components/Curriculo/Curriculo.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 
 function Curriculo(){
+  const secoes = [
+    {
+      titulo:"Resumo",
+      paragrafo1:"Formado em Eletrônica pelo IFAL e em nutrição pela UNIT, decido largar a nutrição e voltar a estudar tecnologia. Em transição de carreira que está estudando para ser um Front End Developer."        
+    },
+    {
+      titulo:"Acadêmico",
+      paragrafo1:"(2009 - 2012) - Técnico em Eletrônica no IFAL.", 
+      paragrafo2:"(2015 - 2019) - Bacharel em Nutrição na UNIT."
+    },
+    {
+      titulo:"Profissional",
+      paragrafo1:"(2012 - Atual) - Técnico em Sistemas Audiovisuais na TV GAZETA.", 
+      paragrafo2:"(2023 - Atual) - Desenvolvedor Front End no Comeia."
+    }
+
+  ]
   return(
     <>
+    {secoes.map((secao) =>
     <section>
-      <h2>Resumo</h2>
-      <p>
-        Formado em Eletrônica pelo IFAL e em nutrição pela UNIT, decido largar a
-        nutrição e voltar a estudar tecnologia. Em transição de carreira que
-        está estudando para ser um Front End Developer.
-      </p>
-    </section>
-    <section>
-      <h2>Acadêmico</h2>
-      <p>(2009 - 2012) - Técnico em Eletrônica no IFAL.</p>
-      <p>(2015 - 2019) - Bacharel em Nutrição na UNIT.</p>
-    </section>
-    <section>
-      <h2>Profissional</h2>
-      <p>(2012 - Atual) - Técnico em Sistemas Audiovisuais na TV GAZETA</p>
-      <p>(2023 - Atual) - Desenvolvedor Front End no Comeia.</p>
-    </section>
+      <h2>{secao.titulo}</h2>
+      <p>{secao.paragrafo1}</p>
+      <p>{secao.paragrafo2}</p>
+    </section>)}
     </>
   )
 }
