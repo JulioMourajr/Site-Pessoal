9c412b6102694145655ae03a1d71a567ca3ac05f
JulioMourajr <juliomourajr@hotmail.com>
2023-05-27 13:20:51 -0300

    Mudança na URL da API

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://json-server-ex-b96v.onrender.com'
+  baseURL: 'https://sitepessoalapi.onrender.com/api'
 })
 
 export default api
